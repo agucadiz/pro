4f8d845f41dfb68e7b682644640be951fa0865ed
Eduardo <martinez.romero.eduardo@gmail.com>
2021-02-22 14:11:16 +0100

    Primera implementación funcional

--- python/ejercicios/programacion_orientada_a_objetos/cliente.py
+++ python/ejercicios/programacion_orientada_a_objetos/cliente.py
@@ -1,9 +1,38 @@
 # Clase Clientes
 # Los clientes son TITULARES de una o varias cuentas
 
-class Cliente():
+class Cliente:
 
     def __init__(self, dni, nombre, apellidos):
-        __dni = dni
-        __nombre = nombre
-        __apellidos = apellidos
+        self.__dni = dni
+        self.__nombre = nombre
+        self.__apellidos = apellidos
+
+    def dni(self):
+        return self.__dni
+
+    def nombre(self):
+        return self.__nombre
+
+    def apellidos(self):
+        return self.__apellidos
+
+    def __repr__(self):
+        return f'Cliente ({self.__nombre} {self.__apellidos}, DNI: {self.__dni})'
+
+
+
+
+
+
+
+
+"""class Alumno(Cliente):
+    def __init__(self, dni, nombre, apellidos, curso):
+        super().__init__(dni, nombre, apellidos)
+        self.__curso = curso
+
+    def descripcion(self):
+        return super().descripcion() + f' y estoy cursando {self.__curso}'
+
+lucia = Alumno ('98765432T', 'Lucía', 'Terraria', 'Prevencion de riesgos laborales')"""
--- python/ejercicios/programacion_orientada_a_objetos/movimiento.py
+++ python/ejercicios/programacion_orientada_a_objetos/movimiento.py
@@ -1,8 +1,28 @@
-# Clase basada en la de interacción entre cuentas
-# Los movimientos son INMUTABLES
-# Los movimientos pertenecen a una sola cuenta
-class Movimiento():
+"""
+Clase estática basada en la de interacción entre cuentas
+Los movimientos son INMUTABLES, por lo que cada movimiento estará dentro de una tupla.
+Los movimientos pertenecen a una sola cuenta
+"""
 
-    def __init__(self, concepto, cantidad):
+class Movimiento:
+
+    @staticmethod
+    def generar_movimiento(concepto, cantidad):
+        """Devuelve una tupla con la estructura (concepto, cantidad)"""
         __concepto = concepto
         __cantidad = cantidad
+        return (__concepto, __cantidad)
+
+    @staticmethod
+    def cantidad(movimiento):
+        return movimiento[1]
+
+
+
+""" Sample code:
+> >> mov1 = Movimiento.generar_movmiento('dietas', -20)
+> >> mov1
+('dietas', -20)
+> >> Movimiento.cantidad(mov1)
+-20
+"""
