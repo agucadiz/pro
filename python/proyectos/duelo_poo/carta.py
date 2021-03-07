f82a796e55d8cfce33f8f3ed982e0c425589802b
Eduardo <martinez.romero.eduardo@gmail.com>
2021-03-07 20:24:30 +0100

    documentada la clase Naipe y relizado un script

--- python/proyectos/duelo_poo/carta.py
+++ python/proyectos/duelo_poo/carta.py
@@ -3,7 +3,7 @@ from random import shuffle as mezclar
 
 class Carta:
     """
-    Cartas
+    Clase Carta
     ---
     La clase Carta representa y almacena las cartas en una baraja.
     La carta está compuesta por:
@@ -11,7 +11,8 @@ class Carta:
         - larga: str -> Descripción larga de la carta (ej: 'Rey de copas')
 
     Ea la baraja, las cartas se identifican por:
-        - int numero; Numero que define en que posición se encuentra la carta dentro de la baraja.
+        - int numero; Numero que define en que posición se encuentra la carta
+        dentro de la baraja.
         - Su valor, compuesto por una carta.
 
     """
@@ -32,8 +33,7 @@ class Carta:
     @staticmethod
     def get_carta(seleccion):
         """
-        Devuelve una carta existente en la baraja a partir de su número.
-
+        Devuelve una carta existente en la baraja a partir de su número.\n
         Parámetros:
             - seleccion: int -> La n-ésima carta de la baraja.
         """
@@ -57,11 +57,11 @@ class Carta:
         return Carta.__baraja
 
     @staticmethod
-    def mostrar_cartas_():
+    def mostrar_baraja():
         """Devuelve de todas las cartas de la baraja."""
-        resultado = ' - '
+        resultado = ''
         for carta in Carta.baraja():
-            resultado += (f'{Carta.describir(carta)} - ')
+            resultado += (f'{Carta.describir(carta)}, ')
         return resultado
 
     @staticmethod
@@ -74,17 +74,29 @@ class Carta:
     @staticmethod
     def describir(seleccion):
         """
-        Devuelve una descripción completa de una carta de la baraja.
+        Devuelve una descripción completa de una carta de la baraja.\n
         Parámetros:
             - seleccion: int -> La n-ésima carta de la baraja.
         """
         seleccion = Carta.get_carta(seleccion)
-        return f'{seleccion.corta()}: {seleccion.larga()}'
+        return f'[{seleccion.corta()}] - {seleccion.larga()}'
 
 
 class Naipe(Carta):
+    """
+    Clase Naipe
+    ---
+    La clase Naipe representa cartas de naipes de la baraja española,
+    las cuáles identifican por su palo y su valor.\n
+    La descripción corta de un naipe tiene esta composición:
+        - El primer caracter del palo : 'Bastos'[0] -> 'B'
+        - El literal del valor : 7 -> '7'
+        Ejemplo: 'Rey de Bastos' ->'B10'\n
+    La descripción larga se trata de un literal en lenguaje natural
+    que describe a la carta:
+        Ejemplo: Naipe('Copas', 8) -> 'Sota de Copas'.
+    """
 
-    # Crear una especie de parser que nos describa la carta en un lenguaje natural, a partir del palo y el valor
     def __init__(self, palo, valor):
         super().__init__(Naipe.generar_corta(palo, valor), Naipe.generar_larga(palo, valor))
         self.__palo = palo
@@ -92,7 +104,7 @@ class Naipe(Carta):
         # self.
 
     def __repr__(self):
-        return f"Naipe('{self.valor()}', '{self.palo()}')"
+        return f"Naipe('{self.palo()}', '{self.valor()}')"
 
     @staticmethod
     def generar_baraja():
@@ -105,13 +117,7 @@ class Naipe(Carta):
 
     @staticmethod
     def generar_corta(palo, valor):
-        """
-        Genera una decripción corta de un naipe, a partir de su palo y valor.
-        La descripción corta de un naipe tiene esta composición:
-            - El primer caracter del palo : 'Bastos'[0] -> 'B'
-            - El literal del valor : 7 -> '7'
-        Ejemplo: 'Rey de Bastos' ->'B10'
-        """
+        """Genera una decripción corta de un naipe, a partir de su palo y valor."""
         palo = palo[0]
         valor = str(valor)
         return palo + valor
@@ -122,16 +128,19 @@ class Naipe(Carta):
         literales = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco', 'Seis', 'Siete','Sota', 'Caballero', 'Rey']
         return f'{literales[valor-1]} de {palo}'
 
+    '''
     @staticmethod
     def mostrar_naipes():
         """Descubre todas las naipes de de la baraja, devolviendo su descripción corta y larga."""
-        resultado = ' - '
+        resultado = ''
         for naipe in Naipe.baraja().values():
             resultado += (f'[{naipe.corta()}] - {naipe.larga()}, ')
         return resultado
+    '''
 
     @staticmethod
     def repartir(cantidad):
+        """Reparte una cantidad de cartas de la baraja, retirandolas de la misma y devolviéndolas."""
         aux = {}
         while cantidad > 0:
             for numero in Naipe.baraja():
@@ -149,12 +158,36 @@ class Naipe(Carta):
         """Devuelve el valor de un naipe."""
         return self.__valor
 
-    def calcular_valor(naipe1, naipe2):
+    def comparar_naipes(naipe1, naipe2):
         """Compara dos cartas y devuelve la de valor superior."""
         if naipe1.valor() > naipe2.valor():
             return naipe1
         else: return naipe2
 
+if __name__ == "__main__":
+    print('Esto es una pequeña demostración de que cómo funciona la clase Carta y, en especial, la subclase Naipe.\n')
+    Naipe.generar_baraja()
+    print(f'Primero vamos a desempolvar la baraja, y vamos a poner todas nuestras cartas boca arriba:\n{Naipe.mostrar_baraja()}\n')
+
+    print(f'Impecable y ordenada. Un detalle interesante es que puedo obtener en todo momento una carta a partir de su posición. Por ejemplo, la carta en la posición 28 es: {Naipe.get_carta(28)}\n')
+    print(f'O mejo aún, podemos obtener una descripción literal de que carta se trata. por ejemplo de la carta 17: {Naipe.describir(17)}\n')
+
+    print('También podemos hacer algo muy interesante. Comparar dos cartas. Comparamos sus valores y nos devuelve la de mayor valor.')
+    print(f'Vamos por ejemplo a comparar las dos cartas que hemos obervado antes, la sota de espadas y el 7 de copas: {Naipe.calcular_valor(Naipe.get_carta(25), Naipe.get_carta(38))}')
+    print('Como era de esperar, nos devuelve la sota, por tener un valor mayor.') # Más adelante incluiré el factor de la "vira", que hara que un palo tenga mayor valor que el resto de naipes
+
+    print('Ahora vamos a barajar las cartas cuidadosamente...')
+    Naipe.barajar()
+    print(f'Y tras barajar, la carta de la posición 28 ya noe s la sota de espadas, ahora es: {Naipe.describir(28)}\n')
+    print(f'Volvamos a ver la baraja, ahora está completamente mezlcada:\n{Naipe.mostrar_baraja()}\n')
+
+    """
+    Repartiremos las 3 cartas primeras cartas que se encuentren en la cima de la baraja y por consiguiente son retiradas de la baraja. Por el momento
+    estas cartas no van a ningún sitio, pero pretendo que la clase Jugador las reciba y guarde con el nombe de "mano".
+    """
+
+    print(f'Vamos a repartir las tres primeras cartas que hay arriba de la baraja:\n{Naipe.repartir(3)}\n')
+    print(f'Ahora si volvemos a ver una vez más la baraja, podremos ver que esas 3 cartas ya no se encuentran:\n{Naipe.mostrar_baraja()}\n')
 '''
 class Yugi(Carta):
     def __init__(self, corta, larga, ataque, defensa):
