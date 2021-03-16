a71a62407d3db9c9f21d3d2a035fff31a60fa2ea
Eduardo <martinez.romero.eduardo@gmail.com>
2021-03-16 12:50:50 +0100

    ejercicios poo: agregada ampliacion a la clase Empleado

--- python/ejercicios/programacion_orientada_a_objetos/empleado/empleado.py
+++ python/ejercicios/programacion_orientada_a_objetos/empleado/empleado.py
@@ -1,3 +1,4 @@
+import unicodedata
 class Empleado:
 
     def __init__(self, nombre, apellidos, salario = 0):
@@ -26,19 +27,27 @@ class Empleado:
         return f'{self.nombre()} {" ".join(apellidos)}'
 
     def email(self):
+
+        """https://stackoverflow.com/questions/517923/what-is-the-best-way-to-remove-\
+        accents-normalize-in-a-python-unicode-string"""
+        def remove_accents(input_str):
+            nfkd_form = unicodedata.normalize('NFKD', input_str)
+            return u"".join([c for c in nfkd_form if not unicodedata.combining(c)])
+
         apellidos = []
         for i in self.apellidos().split():
             apellidos.append(i.lower())
         direccion = f'{self.nombre().lower()}.{".".join(apellidos)}@company.com'
-        return f'{direccion.lower()}'
+        return f'{remove_accents(direccion.lower())}'
 
 if __name__ == '__main__':
     emp1 = Empleado('Juan', 'Pérez')
     emp2 = Empleado('María', 'García')
     emp3 = Empleado('Antonio', 'González')
-    emp1.nombre_completo()
-    emp2.email()
-    emp3.nombre()
-    emp4 = Empleado.desde_cadena('Celia-Atienza Román-55000')
-    emp4.nombre_completo
-    emp4.salario()
+    print(emp1.nombre_completo())
+    print(emp2.email())
+    print(emp3.nombre())
+    emp4 = Empleado.desde_cadena('Celia-Atienza Román-5500')
+    print(emp4.nombre_completo())
+    print(emp4.email())
+    print(emp4.salario())
