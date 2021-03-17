a0334b7f4a0c3edfba3d8a7b2446e3b7bacdfda7
Eduardo <martinez.romero.eduardo@gmail.com>
2021-03-17 12:57:50 +0100

    refuerzo-metodos-magicos: repasado metodo str

--- python/ejercicios/programacion_orientada_a_objetos/persona/persona.py
+++ python/ejercicios/programacion_orientada_a_objetos/persona/persona.py
@@ -26,7 +26,10 @@ class Persona:
 
     def __repr__(self):
         """Devuelve la forma normal de una persona."""
-        return f"Persona('{self.__nombre}', '{self.__dni}'')"
+        return f"Persona('{self.__nombre}', '{self.__dni}')"
+
+    def __str__(self):
+        return f'Se trata de {self.nombre()}, con DNI {self.dni()}.'
 
     def nombre(self):
         """Devuelve el nombre de una persona."""
@@ -65,7 +68,9 @@ print('Vamos a comenzar creando tres instancias de la clase Persona.\n'\
     '\n'
     'En este caso, requerimos de los parámetros "nombre" y "dni".\n'\
     'Al método init también se le denomina "constructor de la clase".\n'\
-        f'     >>> p1 = {p1}\n     >>> p2 = {p2}\n     >>> p3 = {p3}')
+        f'     >>> p1 = {repr(p1)}\n'\
+        f'     >>> p2 = {repr(p2)}\n'\
+        f'     >>> p3 = {repr(p3)}')
 input('Pulse Intro para continuar...')
 print()
 
@@ -88,7 +93,7 @@ print()
 print('----------<<< El método mágico __hash__ >>>----------')
 print('Recuerda que no todos los tipos de datos son hashables. \n'\
     'En el caso de los objetos, a pesar de que éstos son mutables, \n'\
-    'Algunos de sus atributos a menudo suelen permanecer intactos.\n'\
+    'algunos de sus atributos a menudo suelen permanecer intactos.\n'\
     'En esta ocasión definimos un método hash que es elquivalente \n'\
     'a calcular el hash de una lista que contiene el nombre y el \n'\
     'dni de la persona:\n')
@@ -96,21 +101,46 @@ print('Recuerda que no todos los tipos de datos son hashables. \n'\
 print(f'     >>> hash(p1)\n     {hash(p1)}    | Equivale a -> '\
     'hash((p1.__nombre, p1.__dni)) [mirar línea 25]')
 print(f'     >>> hash(p3)\n     {hash(p3)}')
-print(f'     >>>hash(p1) == hash(p3)\n     {hash(p1) == hash(p3)}')
+print(f'     >>> hash(p1) == hash(p3)\n     {hash(p1) == hash(p3)}')
 input('Pulse Intro para continuar...')
 print()
 
 print('----------<<< El método mágico __repr__ >>>----------')
 print('Este método define la forma normal de una instancia de clase, \n'
     'devolviendo una cadena que contiene el estado interno de \n'\
-    'dicha instancia. De este modo, podríamos reconstruir dicho objeto \n'\
-    'a partir de lo que devuelva este método.\n')
+    'dicha instancia. De este modo, podríamos repliclar dicho objeto \n'\
+    'a partir de lo que devuelva este método. De hecho siempre debería \n'\
+    'cumplirse esta condición, pero no siempre es posible.')
 
 print('  - Sin método repr definido:')
 print(f'     >>> p1                     | Equivale a -> Persona.__repr__(p1)'\
     '\n     __main__.Persona object at 0x00000257876D1CD0\n')
 print('  - Con método repr definido:')
-print(f'     >>> p1\n     {p1}')
-print('\nRecuerda que el método se define en la clase, no en la instancia:\n')
-print(f'     >>> Persona.__repr__(p1)\n     {Persona.__repr__(p1)}\n')
-input('Pulse Intro para finalizar...')
+print(f'     >>> p1\n     {p1.__repr__()}')
+input('Pulse Intro para continuar...')
+print()
+
+print('Con la funcion "eval" evaluamos exprexiones contenidas en una cadena.\n'\
+    'Significa que podemos evaluar la cadena que devuelve el método repr:\n')
+
+print(f'     >>> eval(repr(p1)) == p1\n     {eval(repr(p1)) == p1}')
+print(f'     >>> eval(repr(p1)) == p3\n     {eval(repr(p1)) == p3}    | '\
+    'Recuerda que p1 y p3 tenian el mismo estado.')
+
+print('\n¡Recuerda que los métodos se definen en la clase, no en los objetos!\n')
+input('Pulse Intro para continuar...')
+print()
+
+print('----------<<< El método mágico __str__ >>>----------')
+print('Recordar que la función str convierte un valor a cadena. \n'\
+    'Algunos métodos usan esta función de forma implícita, como print.\n'\
+    'El método "objeto.__str__" equivale a hacer str(objeto).\n'\
+    'Este método viene predefinido de manera que __str__ == __repr__, \n'\
+    'pero no debemos olvidar que la finalidad de str es ser legible, mientras \n'\
+    'que repr busca devolver una expresión no ambigüa, la forma normal \n'\
+    'de un objeto. Es por eso que vamos a definir str para que nos devuelva una \n'\
+    'cadena que describa al objeto.\n')
+
+print(f'     >>> print(p1)\n     {p1}')
+print()
+input('Pulse Intro para finalizar.')
