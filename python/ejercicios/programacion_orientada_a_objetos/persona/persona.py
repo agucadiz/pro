fa2005ab68c90d9d257d8f7b9601a8289b1f0cfe
Eduardo <martinez.romero.eduardo@gmail.com>
2021-03-17 12:05:46 +0100

    refuerzo-metodos-magicos: cambiado atributo edad por dni. ampliadas explicaciones

--- python/ejercicios/programacion_orientada_a_objetos/persona/persona.py
+++ python/ejercicios/programacion_orientada_a_objetos/persona/persona.py
@@ -3,12 +3,12 @@ class Persona:
     Clase Persona
     ---
     La clase persona representa a una persona, la cual se \
-    idenfitica por su nombre y su edad.
+    idenfitica por su nombre y su dni.
     """
-    def __init__(self, nombre, edad):
+    def __init__(self, nombre, dni):
         """Constructor de la clase Persona."""
         self.__nombre = nombre
-        self.__edad = edad
+        self.__dni = dni
 
     def __eq__(self, otro):
         """
@@ -18,24 +18,25 @@ class Persona:
         """
         if not isinstance(otro, type(self)):
             return NotImplemented
-        return (self.__nombre, self.__edad) == (otro.__nombre, otro.__edad)
+        return (self.__nombre, self.__dni) == (otro.__nombre, otro.__dni)
 
     def __hash__(self):
         """Devuelve el hash de una persona."""
-        return hash((self.__nombre, self.__edad))
+        return hash((self.__nombre, self.__dni))
 
     def __repr__(self):
         """Devuelve la forma normal de una persona."""
-        return f"Persona('{self.__nombre}', {self.__edad})"
+        return f"Persona('{self.__nombre}', '{self.__dni}'')"
 
     def nombre(self):
         """Devuelve el nombre de una persona."""
         return self.__nombre
 
-    def edad(self):
-        """Devuelve la edad de una persona."""
-        return self.__edad
+    def dni(self):
+        """Devuelve el dni de una persona."""
+        return self.__dni
 
+    '''
     def comparar_edad(self, otra):
         """
         Compara su edad con otra persona, e imprime por \
@@ -46,64 +47,70 @@ class Persona:
         elif self.__edad == otra.edad():
             print (f'{otra.nombre()} tiene la misma edad que yo')
         else: print (f'{otra.nombre()} es más joven que yo')
-
+    '''
 # if __name__ == '__main__':
-p1 = Persona('Paula', 21)
-p2 = Persona('Emilio', 34)
-p3 = Persona('Paula', 21)
+p1 = Persona('Paula', '11122233A')
+p2 = Persona('Emilio', '22233344B')
+p3 = Persona('Paula', '11122233A')
 
 print('\n|          Repaso de los métodos mágicos           |\n')
-print('----------<<<El método mágico __init__>>>----------')
+print('----------<<< El método mágico __init__ >>>----------')
 print('Vamos a comenzar creando tres instancias de la clase Persona.\n'\
     'Ésto se produce gracias al método init que,como su nombre indica,\n'\
     'inicializa los atributos de una clase que se pretende instanciar.\n'\
     'Por lo tanto, habrá que pasarle tantos parámetros como precise.\n'\
     '\n'
-    'Ejemplo de signaruta de un método init:\n'\
-        '     >>> def __init__(self, nombre, edad)\n'\
+    '  - Ejemplo de signaruta de un método init:\n'\
+        '     >>> def __init__(self, nombre, dni)\n'\
     '\n'
-    'En este caso, requerimos de los parámetros "nombre" y "edad".\n'\
+    'En este caso, requerimos de los parámetros "nombre" y "dni".\n'\
     'Al método init también se le denomina "constructor de la clase".\n'\
         f'     >>> p1 = {p1}\n     >>> p2 = {p2}\n     >>> p3 = {p3}')
-input('Pulse cualquier tecla para continuar...')
+input('Pulse Intro para continuar...')
 print()
 
-print('----------<<<El método mágico __eq__>>>----------')
+print('----------<<< El método mágico __eq__ >>>----------')
 print('Por defecto, este método viene predefinido, de manera \n'\
-    'que devolvera True si dos objetos son identicos (is).\n'\
-    ' Nosotros podriamos considerar que dos objetos son iguales \n'\
-    'cuando sus atributos tienen el mismo valor. Por eso hemos \n'\
-    'definido un metodo eq que reconoce que dos personas\n'\
-    'son la misma, si su nombre y edad son iguales. Por eso:\n')
+    'que devolvera True si dos objetos son idénticos (usando \n'\
+    ' el operador "is".\n'\
+    '\n'\
+    'Nosotros podriamos considerar que dos objetos son iguales \n'\
+    'cuando algunos de sus atributos tienen el mismo valor. \n'\
+    'Así que hemos definido un método eq que reconoce que dos personas\n'\
+    'son la misma, si su nombre y dni son iguales. Por eso:\n')
+
 print(f'     >>> p1 is p3\n     {p1 is p3}')
-print(f'     >>> p1 == p3           | En realidad sucede -> '\
-    f'Persona.__eq__(p1, p2)\n     {p1 == p3}')
-input('Pulse cualquier tecla para continuar...')
+print(f'     >>> p1 == p3            | En realidad sucede -> '\
+    f'Persona.__eq__(p1, p3)\n     {p1 == p3}')
+input('Pulse Intro para continuar...')
 print()
 
-print('----------<<<El método mágico __hash__>>>----------')
+print('----------<<< El método mágico __hash__ >>>----------')
 print('Recuerda que no todos los tipos de datos son hashables. \n'\
-    'En el caso de los objetos, a pesar de que estos son mutables, \n'\
+    'En el caso de los objetos, a pesar de que éstos son mutables, \n'\
     'Algunos de sus atributos a menudo suelen permanecer intactos.\n'\
     'En esta ocasión definimos un método hash que es elquivalente \n'\
-    'a calcular el hash de una lista que contiene el nombre y la \n'\
-    'edad de la persona:\n')
-print(f'     >>> hash(p1)\n     {hash(p1)}   | Equivale a -> '\
-    'hash((p1.__nombre, p1.__edad)) [mirar línea 25]')
+    'a calcular el hash de una lista que contiene el nombre y el \n'\
+    'dni de la persona:\n')
+
+print(f'     >>> hash(p1)\n     {hash(p1)}    | Equivale a -> '\
+    'hash((p1.__nombre, p1.__dni)) [mirar línea 25]')
 print(f'     >>> hash(p3)\n     {hash(p3)}')
 print(f'     >>>hash(p1) == hash(p3)\n     {hash(p1) == hash(p3)}')
-input('Pulse cualquier tecla para continuar...')
+input('Pulse Intro para continuar...')
 print()
 
-print('----------<<<El método mágico __repr__>>>----------')
+print('----------<<< El método mágico __repr__ >>>----------')
 print('Este método define la forma normal de una instancia de clase, \n'
-    'devolviendo una expresión que contiene el estado interno de \n'\
-    'dicha instancia. De este modo, podriamos reconstruir dicho objeto.\n')
+    'devolviendo una cadena que contiene el estado interno de \n'\
+    'dicha instancia. De este modo, podríamos reconstruir dicho objeto \n'\
+    'a partir de lo que devuelva este método.\n')
 
 print('  - Sin método repr definido:')
-print(f'     >>> p1\n     __main__.Persona object at 0x00000257876D1CD0\n')
+print(f'     >>> p1                     | Equivale a -> Persona.__repr__(p1)'\
+    '\n     __main__.Persona object at 0x00000257876D1CD0\n')
 print('  - Con método repr definido:')
 print(f'     >>> p1\n     {p1}')
 print('\nRecuerda que el método se define en la clase, no en la instancia:\n')
 print(f'     >>> Persona.__repr__(p1)\n     {Persona.__repr__(p1)}\n')
-input('Pulse cualquier tecla para finalizar...')
+input('Pulse Intro para finalizar...')
